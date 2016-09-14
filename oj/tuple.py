2f25f2c011dfc5998a3d325f919acbf0ab4f1aac
Crayon <chenye626@gmail.com>
2016-09-15 06:52:39 +0800

    use map function

---
+++ oj/tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+# File Name: tuple.py
+# Created Time: Thu Sep 15 06:51:14 2016
+
+__author__ = 'Crayon Chaney <mmmmmcclxxvii@gmail.com>'
+
+#map的用法
+
+raw_input()
+print hash(tuple(map(int,raw_input().split())))
