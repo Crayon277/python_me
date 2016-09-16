52c46ea3ce885a74451fe56014a2dccb418152c7
Crayon <chenye626@gmail.com>
2016-09-16 10:00:19 +0800

    use index & dont use buildin function

--- oj/list_comprehensive_v2.py
+++ oj/list_comprehensive_v2.py
@@ -11,8 +11,17 @@ n = input()
 lis = list(map(int,raw_input().split()))
 m = max(lis)
 
+#max 如果不用内建函数
+m = reduce(lambda x,y:x if x > y else y,lis)
+
 while max(lis) == m:
     lis.remove(m)
 #关键在这里，remove掉所有原本最大的。剩下最大的就是原本第二大了。
 print max(lis)
     
+
+#还有一种
+a = sorted(list(map(int,raw_input().split())))
+w = a.index(max(a))
+#利用index的特性！！！返回第一个出现的值的索引
+print a[w-1]
